d26e6ecea884a188c7ff6a48912a86a7d78fddf7
Benjamin <RichMan@MacBook-Pro-RichMan.local>
2024-07-23 17:47:58 +0500

    4 oy 6 dars

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 let text1 = prompt(`Ismingizni kiriting`);
 let text2 = prompt(`Harf kiriting`);
-let Kichik = text1.toLowerCase();
-let Katta = text1.toUpperCase();
 
-if (Kichik.includes(text2.toLowerCase()) || Katta.includes(text2.toUpperCase())) {
-    alert(`${text1} da ${text2} bor`);
+if (!isNaN(text2)) {
+    text2 = Number(text2);
+    alert(`Son kirgizish mumkin emas`);
 } else {
-    alert(`${text1} da ${text2} yo'q`);
+    if (text1.includes(text2.toLowerCase()) || text1.includes(text2.toUpperCase())) {
+        alert(`${text1} da ${text2} bor`);
+    } else {
+        alert(`${text1} da ${text2} yo'q`);
+    }
 }
